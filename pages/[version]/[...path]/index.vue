54088831372b27f72f3a8dc19081073c13f1bbe4
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-20 21:41:53 +0200

    extract main layout

--- pages/[version]/[...path]/index.vue
+++ pages/[version]/[...path]/index.vue
@@ -1,33 +1,6 @@
 <template>
-  <v-row>
-    <v-col>
-      <VersionCard v-model="version" :path="path"/>
-      <!--      <VersionSelect v-model="version" :path="path"/>-->
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-      <AssetBrowserCard :version="version" :path="path"/>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-      <AdInfoWrapper>
-        <AdWrappper ad-slot="6046448840"/>
-      </AdInfoWrapper>
-    </v-col>
-  </v-row>
-  <!--    <p>{{path}}</p>-->
+  <MainLayout/>
 </template>
 <script setup lang="ts">
-import {useRoute} from 'vue-router';
-import AssetListOrView from "~/components/AssetListOrView.vue";
-import {useVersionPath} from "~/composables/useVersionPath";
-import BreadcrumbsNav from "~/components/BreadcrumbsNav.vue";
-import VersionCard from "~/components/VersionCard.vue";
-
-const route = useRoute();
-
-const {version, path} = useVersionPath();
+import MainLayout from "~/components/MainLayout.vue";
 </script>
-
--- pages/[version]/index.vue
+++ pages/[version]/index.vue
@@ -1,38 +1,6 @@
 <template>
-  <v-row>
-    <v-col>
-      <Jumbotron/>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-<!--      <BreadcrumbsNav v-if="version" :version="version" path="/"></BreadcrumbsNav>-->
-      <VersionCard v-if="version"  v-model="version" :path="[]"/>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-      <AssetBrowserCard v-if="version" :version="version" :path="[]"/>
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-      <AdInfoWrapper>
-        <AdWrappper ad-slot="6046448840"/>
-      </AdInfoWrapper>
-    </v-col>
-  </v-row>
+  <MainLayout/>
 </template>
 <script setup lang="ts">
-import {useGitHubUser} from "~/query/github";
-import {useVersionManifest} from "~/query/misc";
-import VersionSelect from "~/components/VersionSelect.vue";
-import {useRouter} from "#app";
-import AssetListOrView from "~/components/AssetListOrView.vue";
-import BreadcrumbsNav from "~/components/BreadcrumbsNav.vue";
-import {useVersionPath} from "~/composables/useVersionPath";
-import VersionCard from "~/components/VersionCard.vue";
-
-const router = useRouter();
-const {version,path} =useVersionPath();
+import MainLayout from "~/components/MainLayout.vue";
 </script>
