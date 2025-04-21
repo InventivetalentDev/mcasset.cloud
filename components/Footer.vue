a171ea8a8f6e37c165224531c760b76eeec45181
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-21 13:02:49 +0200

    fix links

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,8 +4,8 @@
       <v-row no-gutters justify="center">
         <v-col>
           <v-btn variant="text" href="https://legal.inventivetalent.org/imprint">Imprint</v-btn>
-          <v-btn variant="text" href="https://legal.inventivetalent.org/privacy/mineskin">Privacy</v-btn>
-          <v-btn variant="text" href="https://legal.inventivetalent.org/terms/mineskin">Terms</v-btn>
+          <v-btn variant="text" href="https://legal.inventivetalent.org/privacy">Privacy</v-btn>
+          <v-btn variant="text" href="https://legal.inventivetalent.org/terms">Terms</v-btn>
           <a href="#" id="open_preferences_center">Cookie preferences</a>
         </v-col>
         <v-col class="text-center">
