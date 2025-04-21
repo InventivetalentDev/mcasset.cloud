892f360b65dc8ad4d21c930ee27d4d172ed46774
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-21 13:03:02 +0200

    remove that ig

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,7 +6,6 @@
           <v-btn variant="text" href="https://legal.inventivetalent.org/imprint">Imprint</v-btn>
           <v-btn variant="text" href="https://legal.inventivetalent.org/privacy">Privacy</v-btn>
           <v-btn variant="text" href="https://legal.inventivetalent.org/terms">Terms</v-btn>
-          <a href="#" id="open_preferences_center">Cookie preferences</a>
         </v-col>
         <v-col class="text-center">
           <div class="my-1">Made by <a
