1f8d6be5b06f6157f2a6d47def36d48789906cef
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-21 11:57:06 +0200

    add meta tags + analytics

--- app.vue
+++ app.vue
@@ -9,3 +9,53 @@
     </v-app>
   </NuxtLayout>
 </template>
+<script setup lang="ts">
+
+const description = "MC Assets is a browser for Minecraft asset files. Explore all Textures, Sounds, Models, Fonts, Shaders, and more from any Minecraft version";
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/img/mc-assets-2-64.png'
+    }
+  ],
+  script: [
+    {
+      src: '//cdn.cookie-script.com/s/78e7a161e0aab723429cad3e34a13db5.js',
+      tagPosition: 'head'
+    },
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-HBJZL7F0XS',
+      async: true,
+      'data-cookie-consent': 'tracking',
+      tagPosition: 'head'
+    },
+    {
+      innerHTML: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HBJZL7F0XS');
+            `,
+      'data-cookie-consent': 'tracking',
+      tagPosition: 'head'
+    },
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        "url": "https://mcasset.cloud/",
+        "name": "MC Assets",
+        "alternateName": "mcasset.cloud",
+        "description": description
+      }),
+    }
+  ]
+}, {
+  mode: 'client'
+})
+</script>
