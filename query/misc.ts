7e6aeac446a6baa9cd717b92b83590054f0229b2
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-21 15:03:30 +0200

    change manifest url

--- query/misc.ts
+++ query/misc.ts
@@ -5,7 +5,7 @@ export const useVersionManifest = defineQuery(() => {
     const {state, ...rest} = useQuery({
         key: ['misc','version-manifest'],
         query: async () => {
-            return await $fetch<VersionManifest>('https://cloudflare-worker-cors.inventive.workers.dev/?url=https://piston-meta.mojang.com/mc/game/version_manifest_v2.json')
+            return await $fetch<VersionManifest>('https://assets.mcasset.cloud/manifest.json')
         }
     });
 
