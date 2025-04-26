ce18e23221e7b541d92826ae1765455706e5baef
Haylee Schäfer <haylee@inventivetalent.org>
2025-04-26 11:44:36 +0200

    only debug in dev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,8 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {enabled: true},
     ssr: true,
-    nitro:{
-        debug:true
+    nitro: {
+        debug: process.env.NODE_ENV === 'development'
     },
     experimental: {
         renderJsonPayloads: true
